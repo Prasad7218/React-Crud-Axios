2a5bc12cb11ea4c2f476009449848e3effc1faf6
Prasad7218 <yerawarprasad18@gmail.com>
2022-10-13 15:27:57 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { useEffect,useState } from 'react';
 function App() {
   const[data,setData]=useState([]);
 
+  const[body,setBody]=useState('');
+  const[age,setAge]=useState('');
+
 
   useEffect(()=>{
 
@@ -17,8 +20,23 @@ function App() {
 
   },[])
 
+  const postData=(e)=>{
+    e.preventDefault();
+    Axios.post("http://localhost:3000/comments",{
+      body,
+      age
+    }).then((res)=>{
+      console.log(res);
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+
   return (
     <div className="App">
+      <input type="text" value={body} onChange={(e)=>setBody(e.target.value)}/><br></br>
+      <input type="text" value={age} onChange={(e)=>setAge(e.target.value)}/><br></br>
+      <button onClick={postData}>Post data</button>
       {
         data.map(ele=>{
           return(
@@ -26,6 +44,8 @@ function App() {
           <h1>{ele.id}</h1>
           <h2>{ele.body}</h2>
           <h3>{ele.postId}</h3>
+          <h4>{ele.body}</h4>
+          <h4>{ele.age}</h4>
           </>
           )
         })
